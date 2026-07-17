d7db3422b38d75f89b610577a7c5d0beb3a5e085
sulo1337 <sulochan.acharya2@gmail.com>
2026-07-17 18:09:01 +0000

    test(edition): align community registry assertion

--- internal/edition/edition_test.go
+++ internal/edition/edition_test.go
@@ -5,7 +5,7 @@ package edition
 import "testing"
 
 func TestCommunityRegistryIsEmpty(t *testing.T) {
-	if got := Registry().All(); len(got) != 0 {
+	if got := Registry().Modules(); len(got) != 0 {
 		t.Fatalf("community registry must be empty, got %d extensions", len(got))
 	}
 }
